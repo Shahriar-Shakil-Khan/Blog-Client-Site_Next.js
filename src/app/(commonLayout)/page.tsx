c83f67cb1f70041a15f15fe5e249b1bd32b85352
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-06-27 18:01:50 +0600

    shakil

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { UserService } from "@/services/user.service";
-
-
 export default async function Home() {
- const {data} = await UserService.getSession();
- console.log(data);
   return (
     <div>
       <Button variant="outline">Click Me</Button>
