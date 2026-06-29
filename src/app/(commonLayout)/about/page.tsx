5940350680e17a254c72f48af9192c034d2ff060
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-06-29 12:28:37 +0600

    shakil

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-// export const dynamic= "force-dynamic"; 
+import { getBlogs } from "@/actions/blog.action";
+import { useEffect, useState } from "react";
 
 export default function AboutPage() {
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
 
-  //* For simulating load time
-   //await new Promise((resolve) => setTimeout(resolve, 4000));
+  console.log(data);
+  console.log(error);
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
 
-  //* For simulating error
-   //throw new Error("Something went wrong");
+      setData(data);
+      setError(error);
+    })();
+  }, []);
 
   return (
     <div>
@@ -16,3 +24,9 @@ export default function AboutPage() {
     </div>
   );
 }
+
+//* For simulating load time
+// await new Promise((resolve) => setTimeout(resolve, 4000));
+
+//* For simulating error
+// throw new Error("Something went wrong");
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,4 +1,5 @@
 import { env } from "@/env";
+
 const API_URL = env.API_URL;
 
 //* No Dynamic and No { cache: no-store } : SSG -> Static Page
@@ -18,7 +19,7 @@ interface GetBlogsParams {
 export const blogService = {
   getBlogPosts: async function (
     params?: GetBlogsParams,
-    options?: ServiceOptions
+    options?: ServiceOptions,
   ) {
     try {
       const url = new URL(`${API_URL}/posts`);
@@ -49,6 +50,18 @@ export const blogService = {
       //   if(data.success) {
       //     return
       //   }
+
+      return { data: data, error: null };
+    } catch (err) {
+      return { data: null, error: { message: "Something Went Wrong" } };
+    }
+  },
+
+  getBlogById: async function (id: string) {
+    try {
+      const res = await fetch(`${API_URL}/posts/${id}`);
+
+      const data = await res.json();
 
       return { data: data, error: null };
     } catch (err) {
